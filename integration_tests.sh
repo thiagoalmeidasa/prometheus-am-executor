af71ccba56695c8b45ad781f60ff22ee6c8d93ef
Thiago Almeida <thiagoalmeidasa@gmail.com>
2018-07-27 17:51:58 -0300

    Fix integration tests.

--- integration_tests.sh
+++ integration_tests.sh
@@ -2,9 +2,7 @@
 set -euo pipefail
 readonly ALERT_EXAMPLE='{"receiver":"default","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"InstanceDown","instance":"localhost:1234","job":"broken","monitor":"codelab-monitor"},"annotations":{},"startsAt":"2016-04-07T18:08:52.804+02:00","endsAt":"0001-01-01T00:00:00Z","generatorURL":""},{"status":"firing","labels":{"alertname":"InstanceDown","instance":"localhost:5678","job":"broken","monitor":"codelab-monitor"},"annotations":{},"startsAt":"2016-04-07T18:08:52.804+02:00","endsAt":"0001-01-01T00:00:00Z","generatorURL":""}],"groupLabels":{"alertname":"InstanceDown"},"commonLabels":{"alertname":"InstanceDown","job":"broken","monitor":"codelab-monitor"},"commonAnnotations":{},"externalURL":"http://oldpad:9093","version":"3","groupKey":9777663806026784477}'
 
-go build
-
-TMPFILE=$(tempfile)
+TMPFILE=$(mktemp)
 
 echo "Testing basic command execution, logging to $TMPFILE"
 ./prometheus-am-executor bash -c 'env' > "$TMPFILE" 2>&1 &
